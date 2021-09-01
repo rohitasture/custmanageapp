345886b4dd236df3745a64a546408958712a0e24
Rohit Asture <rohitasture0312@gmail.com>
2021-09-01 16:35:54 +0530

    Added CRUD functionalites

--- custmanageapp/views.py
+++ custmanageapp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 from .models import *
+from .forms import OrderForm
 # Create your views here.
 def home(request):
     orders = Order.objects.all()
@@ -27,3 +28,32 @@ def customer(request, pk):
     context = {'customer':customer, 'orders':orders, 'order_count':order_count}
     return render(request,'custmanageapp/customer.html',context)
 
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request,'custmanageapp/order_form.html',context)
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance = order)
+    if request.method == 'POST':
+        form = OrderForm(request.POST,instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form':form}
+    return render(request,'custmanageapp/order_form.html',context)
+
+def deleteOrder(request,pk):
+    order = Order.objects.get(id=pk)
+    context = {'item':order}
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    return render(request,'custmanageapp/delete.html',context)
